e1d2ae3248953b6faaf67159540234f275955656
Kannika Kabilar <57203769+kannikakabilar@users.noreply.github.com>
2023-01-12 23:00:33 -0500

    c6

---
+++ IQTech/Models/Fact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace IQTech.Models
+{
+	public class Fact
+	{
+		public Fact()
+		{
+		}
+		public int Id { get; set; }
+        public string IqQuestion { get; set; }
+        public string IqAnswer { get; set; }
+    }
+}
+
